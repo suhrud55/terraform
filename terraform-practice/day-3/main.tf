33836587836072fbe2533d14393302fb1cf2b348
Suhrud Valaparla <suhrudvalaparla55@gmail.com>
2025-08-06 18:47:16 +0530

    day-3

--- terraform-practice/day-3/main.tf
+++ terraform-practice/day-3/main.tf
@@ -1,11 +1,40 @@
-resource "aws_instance" "steve" {
-    ami ="ami-0d0ad8bb301edb745"
-    instance_type = "t2.micro"
-    
-  
+
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "ec2-s3-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
-resource "aws_instance" "jack" {
-    ami = "ami-0d0ad8bb301edb745"
-    instance_type = "t2.micro"
-  
+
+
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  role       = aws_iam_role.ec2_s3_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-s3-profile"
+  role = aws_iam_role.ec2_s3_role.name
+
+   
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d54604676873b4ec" 
+  instance_type = "t2.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = "EC2-With-S3-Access"
+  }
 }
