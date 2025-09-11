2ed00b4652f9151c1c2d33c6b342af0a11792996
suhrud55 <suhrudvalaparla55@gmail.com>
2025-09-11 13:01:10 +0530

    Update backend.tf

--- terraform-practice/day-4/backend.tf
+++ terraform-practice/day-4/backend.tf
@@ -1,10 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "showtiem"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-    use_lockfile = true
-    dynamodb_table = "value"
-    encrypt = true
-  }
-}
+
